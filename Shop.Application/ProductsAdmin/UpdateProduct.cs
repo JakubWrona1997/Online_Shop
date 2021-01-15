6bf66ac67566601edaa048b484d464ac89f1d24b
DESKTOP-BK2QFK7\Jakub <jakub.wrona97@gmail.com>
2021-01-15 23:44:16 +0100

    Refactoring models

--- Shop.Application/ProductsAdmin/UpdateProduct.cs
+++ Shop.Application/ProductsAdmin/UpdateProduct.cs
@@ -16,19 +16,27 @@ namespace Shop.Application.ProductsAdmin
             _context = context;
         }
 
-        public async Task Do(ProductViewModel vM)
+        public async Task<Response> Do(Request request)
         {
-          
-
             await _context.SaveChangesAsync();
+            return new Response();
         }
 
-        public class ProductViewModel
+        public class Request
         {
             public string Name { get; set; }
             public string Description { get; set; }
             public decimal Price { get; set; }
         }
+        public class Response
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public decimal Price { get; set; }
+
+        }
+
     }
    
 }
