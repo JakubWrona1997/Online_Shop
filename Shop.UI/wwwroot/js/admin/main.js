07a65b40ff7cb1255941596b00d03d2a540c0e56
JakubWrona <jakub.wrona97@gmail.com>
2021-01-20 01:28:11 +0100

    Added product-manager component

--- Shop.UI/wwwroot/js/admin/main.js
+++ Shop.UI/wwwroot/js/admin/main.js
@@ -1,101 +1,113 @@
 ﻿var app = new Vue({
     el: '#app',
-    data: {
-        loading: false,
-        objectIndex: 0,
-        productModel: {
-            id: 0,
-            name: "Nazwa produktu",
-            description: "Opis",
-            price: 0
-        },
-        products: []
-    },
-    mounted() {
-        this.getProducts();
-    },
-    methods: {
-        getProduct(id) {
-            this.loading = true;
-            axios.get('/Admin/products/' + id)
-                .then(res => {
-                    console.log(res);
-                    var product = res.data;
-                    this.productModel = {
-                        id: product.id,
-                        name: product.name,
-                        description: product.description,
-                        price: product.price
-                    }
-                })
-                .catch(err => {
-                    console.log(err);
-                })
-                .then(() => {
-                    this.loading = false;
-                });
-        },
-        getProducts() {
-            this.loading = true;
-            axios.get('/Admin/products')
-                .then(res => {
-                    console.log(res);
-                    this.products = res.data;
-                })
-                .catch(err => {
-                    console.log(err);
-                })
-                .then(() => {
-                    this.loading = false;
-                });
-        },
-        createProduct() {
-            axios.post('/Admin/products', this.productModel)
-                .then(res => {
-                    console.log(res);
-                    this.products.push(res.data);
-                })
-                .catch(err => {
-                    console.log(err);
-                })
-                .then(() => {
-                    this.loading = false;
-                });
-        },
-        updateProduct() {
-            this.loading = true;
-            axios.put('/Admin/products', this.productModel)
-                .then(res => {
-                    console.log(res);
-                    this.products.splice(this.objectIndex, 1, res.data);
-                })
-                .catch(err => {
-                    console.log(err);
-                })
-                .then(() => {
-                    this.loading = false;
-                });
-        },
-        deleteProduct(id, index) {
-            this.loading = true;
-            axios.delete('/Admin/products/' + id)
-                .then(res => {
-                    console.log(res);
-                    this.products.splice(index, 1);
-                })
-                .catch(err => {
-                    console.log(err);
-                })
-                .then(() => {
-                    this.loading = false;
-                });
-        },
-        editProduct(id, index) {
-            this.objectIndex = index;
-            this.getProduct(id);
-            }
-        },
-
-    computed: {
-        }
+    //data: {
+    //    editing: false,
+    //    loading: false,
+    //    objectIndex: 0,
+    //    productModel: {
+    //        id: 0,
+    //        name: "Nazwa produktu",
+    //        description: "Opis",
+    //        price: 0
+    //    },
+    //    products: []
+    //},
+    //mounted() {
+    //    this.getProducts();
+    //},
+    //methods: {
+    //    getProduct(id) {
+    //        this.loading = true;
+    //        axios.get('/Admin/products/' + id)
+    //            .then(res => {
+    //                console.log(res);
+    //                var product = res.data;
+    //                this.productModel = {
+    //                    id: product.id,
+    //                    name: product.name,
+    //                    description: product.description,
+    //                    price: product.price
+    //                }
+    //            })
+    //            .catch(err => {
+    //                console.log(err);
+    //            })
+    //            .then(() => {
+    //                this.loading = false;
+    //            });
+    //    },
+    //    getProducts() {
+    //        this.loading = true;
+    //        axios.get('/Admin/products')
+    //            .then(res => {
+    //                console.log(res);
+    //                this.products = res.data;
+    //            })
+    //            .catch(err => {
+    //                console.log(err);
+    //            })
+    //            .then(() => {
+    //                this.loading = false;
+    //            });
+    //    },
+    //    createProduct() {
+    //        axios.post('/Admin/products', this.productModel)
+    //            .then(res => {
+    //                console.log(res);
+    //                this.products.push(res.data);
+    //            })
+    //            .catch(err => {
+    //                console.log(err);
+    //            })
+    //            .then(() => {
+    //                this.loading = false;
+    //                this.editing = false;
+    //            });
+    //    },
+    //    updateProduct() {
+    //        this.loading = true;
+    //        axios.put('/Admin/products', this.productModel)
+    //            .then(res => {
+    //                console.log(res);
+    //                this.products.splice(this.objectIndex, 1, res.data);
+    //            })
+    //            .catch(err => {
+    //                console.log(err);
+    //            })
+    //            .then(() => {
+    //                this.loading = false;
+    //                this.editing = false;
+    //            });
+    //    },
+    //    deleteProduct(id, index) {
+    //        this.loading = true;
+    //        axios.delete('/Admin/products/' + id)
+    //            .then(res => {
+    //                console.log(res);
+    //                this.products.splice(index, 1);
+    //            })
+    //            .catch(err => {
+    //                console.log(err);
+    //            })
+    //            .then(() => {
+    //                this.loading = false;
+    //            });
+    //    },
+    //    newProduct() {
+    //        this.editing = true;
+    //        this.productModel.id = 0;
+    //    },
+    //    editProduct(id, index) {
+    //        this.objectIndex = index;
+    //        this.getProduct(id);
+    //        this.editing = true;
+    //    },
+    //    cancel() {
+    //        this.editing = false;
+    //    }
+    //},
+         
+   
+    //computed: {
+    //    }
 });
